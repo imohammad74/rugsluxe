baece72337e04f0e86137636744660a0185dc767
imohammad <m.razmara.1374@gmail.com>
2023-04-09 04:41:30 +0330

    Fix for get URL

--- get_all_brands_url.py
+++ get_all_brands_url.py
@@ -17,26 +17,22 @@ class GetAllBrandsURL:
         """This function is main function in this script and get urls and insert them to database."""
         brand = params['brand']
         main_url = params['url']
-        number_of_product = Common.number_of_product(params['soup'])
-        url_list = Common.last_url(number_of_product)
+        url_list = Common.last_url(params['soup'])
         for url in url_list:
             if url == 0:
                 plp_url = main_url
             else:
-                plp_url = f"{main_url}/{url}"
-            time.sleep(2)
+                plp_url = f"{main_url}&p={url}"
             r = requests.get(plp_url)
             soup = BeautifulSoup(r.content, "html.parser")
-            a_tags = soup.find_all(class_='rd-product-data')
+            a_tags = soup.find_all(class_='product-item-link')
             for pdp_url in a_tags:
                 current_time = int(now.strftime("%y%m%d"))
-                ss = BeautifulSoup(str(pdp_url), "html.parser")
-                el = ss.find(class_='style', href=True)
-                clean_url = Common.get_url(el)
+                url = pdp_url['href']
                 db.insert_rows(db_file=db.db_file(), table_name=db.db_table()[0], log=False, columns=[
                     {
                         'column': 'url_address',
-                        'value': clean_url,
+                        'value': url,
                     },
                     {
                         'column': 'last_update',
@@ -47,7 +43,7 @@ class GetAllBrandsURL:
                         'value': brand,
                     }
                 ])
-            print(f'{url} finish!')
+        print(f'{brand} finish!')
 
     def __init__(self, params):
         self.main(params)
